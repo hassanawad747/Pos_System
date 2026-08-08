b57e1b4abb16c7bcd3562d0606fdb80e4665264f
hassan awwad <74962890+hassanawad747@users.noreply.github.com>
2026-08-09 00:33:47 +0300

    Register stock loss module in dependency injection

--- Services/AppServices.cs
+++ Services/AppServices.cs
@@ -33,19 +33,18 @@ namespace Pos_System.Services
                 if (provider != null) return;
                 string cs = POS_System.Program.SettingsManager.ConnectionString;
                 var services = new ServiceCollection();
-
                 services.AddSingleton(new SalesLifecycleService(cs));
                 services.AddSingleton(new InventoryOperationsService(cs));
+                services.AddSingleton(new StockLossService(cs));
                 services.AddSingleton(new PricingService(cs));
                 services.AddSingleton(new BusinessIntelligenceService(cs));
                 services.AddSingleton(new WhishPaymentService());
-
                 services.AddTransient<SalesLifecycleForm>();
                 services.AddTransient<InventoryOperationsForm>();
+                services.AddTransient<StockLossForm>();
                 services.AddTransient<PricingAdminForm>();
                 services.AddTransient<BusinessIntelligenceForm>();
                 services.AddTransient<SecurityAdminForm>();
-
                 provider = services.BuildServiceProvider();
             }
         }
